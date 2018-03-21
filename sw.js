ffb4d51c4586b7a106d6755ae0f3ba07d35dd7c2
Erdoğan Öksüz <erdoganoksuz95@gmail.com>
2018-03-21 21:33:21 +0300

    sw test

---
+++ sw.js
@@ -0,0 +1,33 @@
+// sw.js
+// listen for outgoing network request
+self.addEventListener('fetch', (event) => {
+    // try to find response object in the cache
+    // associated with current request
+    event.respondWith(caches.match(event.request)
+      .then((cachedResponse) => {
+        // if there's cached response, give it back
+        if (cachedResponse) {
+  
+          return cachedResponse;
+        }
+        // if no, try to fetch it from the network
+        return fetch(event.request.clone())
+          .then((networkResponse) => {
+            // if response is “bad”,
+            // just pass it back into the app
+            if (!networkResponse || networkResponse.status !== 200 ||
+                networkResponse.type !== 'basic') {
+  
+              return networkResponse;
+            }
+  
+            // if response is ok, cache it and
+            // give it back into the app
+            caches.open(OFFLINE_CACHE)
+              .then((cache) => cache.put(
+                event.request, networkResponse.clone()));
+  
+            return networkResponse;
+          });
+      }));
+  });
