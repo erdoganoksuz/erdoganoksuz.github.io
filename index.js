e7819c99d3184f4412fa91bc47c7c39be3b86e7e
Erdogan Oksuz <erdoganoksuz95@gmail.com>
2020-04-17 15:13:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const $box = document.getElementsByClassName('box')[0];
 
-var onChange = function ({
-    beta,
-    gamma
-}) {
-    $box.style.top = `${Math.floor(beta)}%`;
-    $box.style.left = `${Math.floor(gamma)}%`;
+const $input=document.querySelector("input");
 
-}
 
-window.addEventListener("deviceorientation", onChange, true);
+$input.addEventListener("input",(e)=>{
+  console.log(e.target.value)
+			const amount = e.target.value.replace(",",".");
+	    const matched = amount.match(/^([0-9]{0,3})(\.|)([0-9]{0,2})/g);
+    if (Array.isArray(matched)) {
+      if (Number(matched[0]) > 750) {
+        //setAmount(wallet.remainingDepositLimit.toString());
+				$input.value= 750;
+        return false;
+      } else $input.value= matched[0];
+    }
+    setInputActive(inputActive);
+    return true;
+})
